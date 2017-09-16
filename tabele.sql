dd98a5e4836ec88405b2207c192ab9cd7ea26a7c
Lukasz Guszcz <lukaszguszcz@gmail.com>
2017-09-16 15:59:52 +0200

    wszystkie inserty

--- tabele.sql
+++ tabele.sql
@@ -2,15 +2,23 @@ create database tabele;
 #drop database tabele;
 use tabele;
 
-#drop table klienci
+#drop table klienci;
 
 CREATE TABLE klienci (
-    id_k INT,
+    id_k INT AUTO_INCREMENT,
     nazwa TEXT,
     NIP TEXT,
     PRIMARY KEY (id_k)
 );
+drop table produkty;
+CREATE TABLE produkty (
+    id_p INT auto_increment,
+    nazwa_p TEXT,
+    cena float,
+    PRIMARY KEY (id_p)
+);
 
+drop table opiekunowie;
 CREATE TABLE opiekunowie (
     id_o INT AUTO_INCREMENT,
     id_k INT,
@@ -18,20 +26,11 @@ CREATE TABLE opiekunowie (
     nazwa_o TEXT,
     PRIMARY KEY (id_o),
     FOREIGN KEY (id_k)
-        REFERENCES klienci,
+        REFERENCES klienci (id_k),
     FOREIGN KEY (id_p)
-        REFERENCES produkty
+        REFERENCES produkty (id_p)
 );
-#drop table opiekunowie
 
-
-CREATE TABLE produkty (
-    id_p INT,
-    nazwa_p TEXT,
-    cena float,
-    PRIMARY KEY (id_p)
-);
-#drop table produkty
 
 
 INSERT INTO klienci (nazwa, NIP) VALUES ('Krawczyk', '5431234398');
@@ -40,20 +39,28 @@ INSERT INTO klienci (nazwa, NIP) VALUES ('Gorniak', '5431234000');
 INSERT INTO klienci (nazwa, NIP) VALUES ('Piaseczny', '1231238765');
 INSERT INTO klienci (nazwa, NIP) VALUES ('Dzikus', '5431234300');
 
-
-#drop table opiekunowie
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('1', '11', 'Kon');
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('2', '21', 'Kaczka');
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('3', '31', 'Koza');
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('4', '41', 'Krowa');
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('5', '51', 'Alpaka');
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('5', '41', 'Alpaka');
-INSERT INTO opiekunowie (id_k, id_p, nazwa) VALUES ('5', '31', 'Alpaka');
-
 INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('11', 'Kopytka', 4);
 INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('21', 'Skrzydelka', 10);
 INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('31', 'Rogi', 4);
 INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('41', 'Mleko', 1);
 INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('51', 'Futro', 100);
+INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('61', 'Jajo', 2);
+INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('71', 'Fejm', 9);
+INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('81', 'Lans', 7);
+INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('91', 'Parowka', 12);
+INSERT INTO produkty (id_p, nazwa_p, cena) VALUES ('101', 'Motorowka', 67);
+
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('1', '11', 'Kon');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('2', '21', 'Kaczka');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('3', '31', 'Koza');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('4', '41', 'Krowa');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('5', '51', 'Alpaka');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('5', '41', 'Alpaka');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('5', '31', 'Alpaka');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('4', '91', 'Krowa');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('3', '81', 'Koza');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('2', '71', 'Kaczka');
+INSERT INTO opiekunowie (id_k, id_p, nazwa_o) VALUES ('1', '101', 'Kon');
+
 
 
