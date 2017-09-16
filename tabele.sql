307e5d6fb99e2ebe7cd36ddb85baa99a4734b915
wesol <mwesolowski88@gmail.com>
2017-09-16 15:13:28 +0200

    dodanie tabeli opiekunowie

--- tabele.sql
+++ tabele.sql
@@ -5,7 +5,18 @@ use tabele;
 CREATE TABLE klienci (
     id_k INT,
     nazwa TEXT,
-    NIP TEXT
+    NIP TEXT,
+    primary key(id_k)
 );
 
 #drop table klienci
+
+CREATE TABLE opiekunowie (
+    id_o INT auto_increment,
+    id_k int,
+    id_p int,
+    nazwa TEXT,
+    primary key(id_o),
+    foreign key (id_k) references klienci
+);
+#drop table opiekunowie
