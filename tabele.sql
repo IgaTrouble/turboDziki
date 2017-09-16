d93d7a077d9078bce23775ad4ead3d9d4f708d18
wesol <mwesolowski88@gmail.com>
2017-09-16 15:01:06 +0200

    od L2

--- tabele.sql
+++ tabele.sql
@@ -1,13 +1,26 @@
+<<<<<<< HEAD
 drop table tabele;
 
 CREATE TABLE tabele (
     id_k INT primary key auto_increment,
+=======
+create database tabele;
+#drop database tabele;
+use tabele;
+
+CREATE TABLE klienci (
+    id_k INT,
+>>>>>>> c6d77a1... aktualizacja tabeli 1
     nazwa TEXT,
     NIP TEXT
 );
 
+<<<<<<< HEAD
 INSERT INTO tabele (nazwa, NIP) VALUES ('Krawczyk', '5431234398');
 INSERT INTO tabele (nazwa, NIP) VALUES ('Santor', '5431223398');
 INSERT INTO tabele (nazwa, NIP) VALUES ('Gorniak', '5431234000');
 INSERT INTO tabele (nazwa, NIP) VALUES ('Piaseczny', '1231238765');
 INSERT INTO tabele (nazwa, NIP) VALUES ('Dzikus', '5431234300');
+=======
+#drop table klienci
+>>>>>>> c6d77a1... aktualizacja tabeli 1
