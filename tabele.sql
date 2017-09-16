f235137a47ce2ee85292fe35cba0b973b79be660
Lukasz Guszcz <lukaszguszcz@gmail.com>
2017-09-16 15:09:06 +0200

    od Lukasza

--- tabele.sql
+++ tabele.sql
@@ -1,5 +1,13 @@
+drop table tabele;
+
 CREATE TABLE tabele (
-    id_k INT,
+    id_k INT primary key auto_increment,
     nazwa TEXT,
     NIP TEXT
 );
+
+INSERT INTO tabele (nazwa, NIP) VALUES ('Krawczyk', '5431234398');
+INSERT INTO tabele (nazwa, NIP) VALUES ('Santor', '5431223398');
+INSERT INTO tabele (nazwa, NIP) VALUES ('Gorniak', '5431234000');
+INSERT INTO tabele (nazwa, NIP) VALUES ('Piaseczny', '1231238765');
+INSERT INTO tabele (nazwa, NIP) VALUES ('Dzikus', '5431234300');
